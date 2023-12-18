0ff9c51fbd21409ce7cd920cb8ea1901923e6f2f
yash1378 <slayycasm997@gmail.com>
2023-12-18 13:08:25 +0530

    tracker and enroll page added

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,17 +1,19 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-
-
 export default NextAuth({
-    // Configure one or more authentication providers
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-        }),
-
-        // ...add more providers here
-    ],
-
-})
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    // Add more providers as needed
+  ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      // Redirect to another page after successful sign-in
+      return Promise.resolve('/intro'); // Specify the path to your desired redirect page
+    },
+  },
+  // ...other configuration options
+});
---
+++ pages/tracker.tsx
@@ -0,0 +1,90 @@
+// pages/test-tracker.tsx
+
+import React, { useState } from 'react';
+import { Typography, Container, FormControl, InputLabel, Select, MenuItem, TextField, Button } from '@mui/material';
+import { styled } from '@mui/system';
+import { set } from 'date-fns';
+const StyledButton = styled(Button)({
+
+    backgroundColor: '#2196F3', // Set your desired background color
+    color: '#1565C0', // Set your desired text color
+    fontSize: '20px', // Set your desired font size
+    width: '100%', // Set the width to 100% to match the fields
+    marginTop:'16px',
+    '&:hover': {
+      backgroundColor: '#1565C0', // Set a different color on hover if needed
+      color:'white'
+    },
+  });
+  
+const TestTracker: React.FC = () => {
+  const [testType, setTestType] = useState('');
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const handleTestTypeChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setTestType(event.target.value as string);
+  };
+
+  const handleDateChange = (date: Date | null) => {
+    if (date) {
+      setSelectedDate(date);
+    }
+  };
+
+  const handleSubmit = () => {
+    setTestType('');
+    setSelectedDate(new Date());
+    console.log('Test Type:', testType);
+    console.log('Selected Date:', selectedDate);
+    // Add any additional submission logic here
+  };
+
+  return (
+    <Container
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh', // Adjust if needed to center vertically in the viewport
+      }}
+    >
+      <Typography variant="h4" gutterBottom>
+        Create Your Test Tracker
+      </Typography>
+      <form style={{ width: '100%', maxWidth: '400px' }}>
+        <FormControl fullWidth variant="outlined" margin="normal">
+          <InputLabel id="test-type-label">Test Type</InputLabel>
+          <Select
+            label="Test Type"
+            id="test-type"
+            value={testType}
+            onChange={handleTestTypeChange}
+          >
+            <MenuItem value="mains">Class Mains</MenuItem>
+            <MenuItem value="advanced">Classic Advanced</MenuItem>
+            <MenuItem value="custom">Custom Test</MenuItem>
+          </Select>
+        </FormControl>
+        <TextField
+          label="Select Date"
+          type="date"
+          id="test-date"
+          value={selectedDate.toISOString().split('T')[0]} // Format date as 'YYYY-MM-DD'
+          onChange={(e) => handleDateChange(new Date(e.target.value))}
+          variant="outlined"
+          margin="normal"
+          fullWidth
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
+        <StyledButton variant="contained" color="primary" onClick={handleSubmit}>
+          Submit
+        </StyledButton>
+      </form>
+    </Container>
+  );
+};
+
+export default TestTracker;
